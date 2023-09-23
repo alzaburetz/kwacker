f10b68164c6cdc94bb369acf7c3343adb6962b9f
Anton Motin <anton.motin.dev@gmail.com>
2023-09-24 02:50:37 +0300

    Added serializer and test

---
+++ handlers/serializers/user_serializer.go
@@ -0,0 +1,36 @@
+package serializers
+
+import (
+	"encoding/json"
+
+	"github.com/alzaburetz/kwacker/models"
+	"github.com/alzaburetz/kwacker/transport"
+)
+
+func SerializeUser(user models.User) (string, error) {
+	if !user.IsValidated {
+		panic("User must be validated")
+	}
+
+	transportUser := transport.User{
+		Name:            user.Name,
+		Surname:         user.Surname,
+		Posts:           user.Posts,
+		NickNameHandler: user.NickNameHandler,
+	}
+	result, err := json.Marshal(transportUser)
+
+	return string(result), err
+}
+
+func SerializeUsers(users []models.User) (string, error) {
+	for _, user := range users {
+		if !user.IsValidated {
+			panic("User must be validated")
+		}
+	}
+
+	result, err := json.Marshal(users)
+
+	return string(result), err
+}
---
+++ handlers_tests/serializers/user_serializer_test.go
@@ -0,0 +1,22 @@
+package serializers_test
+
+import (
+	"testing"
+
+	"github.com/alzaburetz/kwacker/handlers/serializers"
+	"github.com/alzaburetz/kwacker/models"
+)
+
+func TestSerializeUserWorksCorrectly(t *testing.T) {
+	// Arrange
+	user, _ := models.NewUser("John", "Doe", "john", 2)
+	expectedJson := "{\"name\":\"John\",\"surname\":\"Doe\",\"nick_name\":\"john\",\"posts\":2}"
+
+	// Act
+	result, err := serializers.SerializeUser(*user)
+
+	// Assert
+	if result != expectedJson || err != nil {
+		t.Errorf("Wanted: %v, Got: %v", expectedJson, result)
+	}
+}
